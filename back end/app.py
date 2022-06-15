f41609718f53c166ef8a33f172f3766e16065c9d
arthurbugarin <arthur.mello.marques@gmail.com>
2022-06-14 22:42:36 -0300

    changed backend to flask with socketio

---
+++ back end/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send
+
+import chatroom
+import member
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+chat_room = chatroom.ChatRoom()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on("message")
+def handle_message(msg):
+    print(msg)
+    emit("message", "Server response: " + msg)
+
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+    new_member = member.Member(request.sid, "nomedetesteeeeeeeeee")
+    new_member.send_message('Bem vindo ao chat!')
+    chat_room.add_member(new_member)
+    chat_room.broadcast_system(new_member.name + ' entrou na sala!')
+    emit("server message",
+         "Bem vindo ao chat! Digite /nome [seu nome] para definir seu nome e entrar na sala")
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+    chat_room.delete_member(request.sid)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
--- back end/chatroom.py
+++ back end/chatroom.py
@@ -1,20 +1,24 @@
 import member
 import message
+from flask_socketio import emit
+
 
 class ChatRoom():
     # TODO: change this to simple array of Member objects
-    members = {} # name: websocket
+    members = {}  # name: socket_id
 
     def __init__(self):
         return
 
-    async def add_member(self, member: member.Member):
+    def add_member(self, member: member.Member):
         if not self.member_in_room(member.name):
-            self.members[member.name] = member.websocket
-            await member.send_system_message('Seja bem vindo à conversa, ' + member.name + '!' + '\n' + 'Digite /nome [novo nome] para mudar seu nome')
-            await self.broadcast_system(member.name + ' entrou na sala!')
+            self.members[member.name] = member.socket_id
+            member.send_system_message('Seja bem vindo à conversa, ' + member.name +
+                                       '!' + '\n' + 'Digite /nome [novo nome] para mudar seu nome')
+            self.broadcast_system(member.name + ' entrou na sala!')
             return
-        await member.send_system_message('Já existe um participante com esse nome! Escolha outro.')
+        member.send_system_message(
+            'Já existe um participante com esse nome! Escolha outro.')
 
     def member_in_room(self, name: str):
         return name in self.members
@@ -22,20 +26,15 @@ class ChatRoom():
     def get_member(self, name: str):
         return member.Member(name, self.members[name])
 
-    def delete_member(self, member: member.Member):
-        del self.members[member.name]
+    def delete_member(self, socket_id: str):
+        del self.members[list(self.members.keys())[list(
+            self.members.values()).index(socket_id)]]
 
-    async def broadcast(self, message: message.Message):
-        if not self.member_in_room(message.sender.name):
-            await message.sender.send_system_message('Você não está na sala!')
-            return
-        for name in self.members:
-            if name != message.sender.name:
-                await self.members[name].send(message.sender.name + ": " + message.text)
+    def broadcast(self, message: message.Message):
+        emit("message", message.text, broadcast=True, include_self=False)
 
-    async def broadcast_system(self, text: str):
-        for name in self.members:
-            await self.members[name].send("Servidor: " + text)
+    def broadcast_system(self, text: str):
+        emit("server message", text, broadcast=True, include_self=True)
 
     def get_names(self):
         return self.members.keys()
